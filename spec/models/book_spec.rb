ea3dc786a1633ccdddc4f36f02d709052111b2bc
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-29 14:32:25 +0900

    Refactor: have_attributesマッチャの利用

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,21 +3,17 @@ require 'rails_helper'
 RSpec.describe Book, type: :model do
   let(:book) { FactoryBot.create(:book) }
   context "タイトルと内容と筆者" do
-    it 'タイトル' do
-      expect(book[:title]).to eq 'A book'
-    end
-
-    it "内容" do
-      expect(book[:memo]).to eq 'The best simple book'
-    end
-
-    it "著者" do
-      expect(book[:author]).to eq 'Rob'
+    it "タイトルと内容、著者が設定できること" do
+      expect(book).to have_attributes(
+        title: 'A book',
+        memo: 'The best simple book',
+        author: 'Rob'
+      )
     end
   end
 
   context "画像" do
-    it do
+    it "画像が設定できること" do
       expect(book[:picture]).to eq 'image.png'
     end
   end
