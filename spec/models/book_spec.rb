f0143936a160bd144e28877749bbab0352ad7f78
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-29 13:57:22 +0900

    refactor: スペックのインスタンスメソッドをletで置換

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  before {@book = FactoryBot.create(:a_book)}
+  let(:book) { FactoryBot.create(:a_book) }
   context "タイトルと内容と筆者" do
     it 'タイトル' do
-      expect(@book[:title]).to eq 'A book'
+      expect(book[:title]).to eq 'A book'
     end
 
     it "内容" do
-      expect(@book[:memo]).to eq 'The best simple book'
+      expect(book[:memo]).to eq 'The best simple book'
     end
 
     it "著者" do
-      expect(@book[:author]).to eq 'Rob'
+      expect(book[:author]).to eq 'Rob'
     end
   end
 
   context "画像" do
     it do
-      p @book
-      expect(@book[:picture]).to eq 'image.png'
+      p book
+      expect(book[:picture]).to eq 'image.png'
     end
   end
 
